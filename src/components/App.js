a2f222bb2608c5b0d37e216e061ef731e1ebc758
clementia1 <grininma0@gmail.com>
2018-05-15 15:54:45 +0300

    tabs

--- src/components/App.js
+++ src/components/App.js
@@ -11,19 +11,22 @@ class App extends React.Component {
         return (
             <div>
                 <div className="container">
-                    <div className="row">
-                        <div className="col-sm">
-                        One of three columns
-                        <Slider  />
+                    <ul className="nav nav-tabs nav-justified " id="myTab" role="tablist">
+                        <li className="nav-item">
+                            <a className="nav-link active" id="first-tab" data-toggle="tab" href="#first" role="tab" aria-controls="first" aria-selected="true">First Tab</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" id="second-tab" data-toggle="tab" href="#second" role="tab" aria-controls="second" aria-selected="false">Second Tab</a>
+                        </li>
+                    </ul>
+
+                    <div className="tab-content" id="myTabContent">
+                        <div className="tab-pane fade show active" id="first" role="tabpanel" aria-labelledby="first-tab">
+                        1 <Slider/>                        
                         </div>
-                        <div className="col-sm">
-                        One of three columns
-                        </div>
-                        <div className="col-sm">
-                        One of three columns
-                        </div>
+                        <div className="tab-pane fade" id="second" role="tabpanel" aria-labelledby="second-tab">2</div>
                     </div>
-                </div>               
+                </div>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
+import './scss/custom.scss';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
