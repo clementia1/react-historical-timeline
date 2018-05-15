766684492420d653cdf14193bae073a28056e120
clementia1 <grininma0@gmail.com>
2018-05-15 14:58:41 +0300

    bootstrap js and styles

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,20 @@ class App extends React.Component {
     render() {
         return (
             <div>
-               <Slider  />
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm">
+                        One of three columns
+                        <Slider  />
+                        </div>
+                        <div className="col-sm">
+                        One of three columns
+                        </div>
+                        <div className="col-sm">
+                        One of three columns
+                        </div>
+                    </div>
+                </div>               
             </div>
         );
     }
