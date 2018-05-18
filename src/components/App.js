d6218e6e78e1cecb81395734b05c7b81703e3117
clementia1 <grininma0@gmail.com>
2018-05-18 09:00:46 +0300

    basic design

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,49 @@ class App extends React.Component {
     constructor() {
         super();
     }
-    
+
     render() {
+        const style = {
+            width: 'auto',
+            margin: 50
+        };
+        const marks = {
+            5: {
+                style: {
+                },
+                label: 'March 1939'
+            },
+            15: {
+                style: {                   
+                },
+                label: 'April 1939'
+            },
+            30: {
+                style: {                    
+                },
+                label: <strong>May 1939</strong>
+            },
+            45: {
+                style: {                   
+                },
+                label: 'May 1941'
+            },
+            70: {
+                style: {                    
+                },
+                label: 'June 1941'
+            },
+            100: {
+                style: {
+                    color: 'red',                    
+                },
+                label: <strong>July 1941</strong>,
+            },
+        };
+
         return (
             <div>
-                <div className="container">
+                <div className="container-fluid">
                     <ul className="nav nav-tabs nav-justified " id="myTab" role="tablist">
                         <li className="nav-item">
                             <a className="nav-link active" id="first-tab" data-toggle="tab" href="#first" role="tab" aria-controls="first" aria-selected="true">First Tab</a>
@@ -22,7 +60,28 @@ class App extends React.Component {
 
                     <div className="tab-content" id="myTabContent">
                         <div className="tab-pane fade show active" id="first" role="tabpanel" aria-labelledby="first-tab">
-                        1 <Slider/>                        
+                            <div className="row">
+                                <div className="col">
+
+                                    <div class="form-row">
+                                        <div class="col">
+                                        <input type="text" class="form-control" placeholder="Event title"/>
+                                        </div>                                        
+                                    </div>
+                                    <div class="form-row">
+                                        <div class="col">
+                                        <input type="text" class="form-control" placeholder="Event summary"/>
+                                        </div>                                        
+                                    </div>
+                                </div>
+                                <div className="col">
+                                Preview area
+                                    <div style={style}>                                
+                                        <Slider min={0} marks={marks} step={null} defaultValue={0} />
+                                    </div>                                  
+                                </div>
+                            </div>
+                   
                         </div>
                         <div className="tab-pane fade" id="second" role="tabpanel" aria-labelledby="second-tab">2</div>
                     </div>
